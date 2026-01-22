80b2f82df9346295b19b0903784c29b20c5b9efa
Jorge Oliveira <jrge.rbrt@gmail.com>
2026-01-22 13:57:41 -0300

    primeira modificação de codigo

--- PyLearn-main/Aula008.py
+++ PyLearn-main/Aula008.py
@@ -6,5 +6,5 @@ print(f'A raiz de {num} é igual a {trunc(raiz)}')
 import random
 numero = random.random()
 print(numero)
-_um_a_dez = random.randint(1, 10)
+_um_a_dez = random.randint(1, 100)
 print(_um_a_dez)
