d3fb4e83c4a2f8f5b8a3e2ca3c0b87a853830e56
Jorge Oliveira <jrge.rbrt@gmail.com>
2026-02-03 14:19:29 -0300

    Diminuindo o range

--- PyLearn-main/ex48.py
+++ PyLearn-main/ex48.py
@@ -1,6 +1,6 @@
 print('-=-' * 40)
-print('Todos os números ímpares e multiplos de 3 entre 1 e 500')
+print('Todos os números ímpares e multiplos de 3 entre 1 e 400')
 print('-=-' * 40)
-for i in range(1, 501):
+for i in range(1, 401):
     if i % 2 != 0 and i % 3 == 0:
         print(i)
